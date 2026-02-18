c27cb4b252b36d66ecb94d981d2e21084e2ab6fa
Tejadorasetti <thedevops.learnerxgmail.com>
2026-02-18 08:25:40 +0530

    common

---
+++ catalogue.sh
@@ -0,0 +1,30 @@
+#! bin/bash
+
+source ./common.sh
+
+app_name="catalogue"
+check_root
+app_setup
+nodejs_setup
+
+
+# mongodb setup
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGS_FILE
+VALIDATE $? "copying mongo repo file"
+
+sudo dnf install mongodb-mongosh -y &>>$LOGS_FILE
+VALIDATE $? "installing mongodb mongosh client"
+
+
+Index=$(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")') &>>$LOGS_FILE
+
+if [ $Index -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOGS_FILE
+    VALIDATE $? "loading products data to catalogue database"
+else
+    echo -e "$Y $(date "+%y-%m-%d %H:%M:%S")| Products already loaded in catalogue database $Y SKIPPING $N" | tee -a $LOGS_FILE
+fi
+
+app_restart
+
--- common.sh
+++ common.sh
@@ -2,14 +2,14 @@
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/Shell-Roboshop"
-SCRIPT_NAME=$(basename "$0")
-SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$pwd
+LOGS_FILE="$LOGS_FOLDER/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 START_TIME=$(date +%s)
+MONGODB_HOST=mongodb.learn-devops.cloud
 
 echo "script started excuting at: $(date "+%y-%m-%d %H:%M:%S")"  | tee -a $LOGS_FILE
 
@@ -35,14 +35,60 @@ VALIDATE(){
 
 }
 nodejs_setup(){
-    dnf module disable nodejs -y &>>$LOGS_FILE
-VALIDATE $? "disabling nodejs module"
+        dnf module disable nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "disabling nodejs module"
 
-dnf module enable nodejs:20 -y &>>$LOGS_FILE
-VALIDATE $? "enabling nodejs 20 module"
+    dnf module enable nodejs:20 -y &>>$LOGS_FILE
+    VALIDATE $? "enabling nodejs 20 module"
 
-dnf install nodejs -y &>>$LOGS_FILE
-VALIDATE $? "installing nodejs"
+    dnf install nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "installing nodejs"
+
+    npm install &>>$LOGS_FILE
+    VALIDATE $? "installing catalogue dependencies"
+}
+
+app_setup(){
+
+        id roboshop &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+        VALIDATE $? "adding roboshop user"
+
+    else
+        echo -e "$Y roboshop user already exists, skipping user creation $N" | tee -a $LOGS_FILE
+    fi  
+
+    cd /app &>>$LOGS_FILE
+    VALIDATE $? "navigating to application directory"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOGS_FILE
+    VALIDATE $? "downloading $app_name code"
+
+    rm -rf /app/* &>>$LOGS_FILE
+    VALIDATE $? "cleaning application directory"
+
+    unzip /tmp/$app_name.zip &>>$LOGS_FILE
+    VALIDATE $? "extracting $app_name code" 
+}
+
+system_steup(){
+    cp "$SCRIPT_DIR/$app_name.service" /etc/systemd/system/$app_name.service &>>$LOGS_FILE
+    VALIDATE $? "copying $app_name systemd service file"
+
+    systemctl daemon-reload &>>$LOGS_FILE
+    VALIDATE $? "reloading systemd daemon"
+
+    systemctl enable $app_name &>>$LOGS_FILE
+    VALIDATE $? "enabling $app_name service"
+
+    systemctl start $app_name &>>$LOGS_FILE
+    VALIDATE $? "starting $app_name service"
+}
+
+app_restart(){
+    systemctl restart $app_name &>>$LOGS_FILE
+    VALIDATE $? "restarting $app_name service"
 }
 
 print_total_time(){
